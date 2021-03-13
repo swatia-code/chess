b60eb8b726b91f70a9890e34ae61cd5d43e32f17
Swati Arora <61271503+swatia-code@users.noreply.github.com>
2021-03-13 16:27:36 +0530

    Create player.py

---
+++ player.py
@@ -0,0 +1,17 @@
+"""To encapsulate a chess player"""
+from render import ConsoleRender
+from game import ChessGame
+
+
+class Player:
+    def play_chess(self):
+        render = ConsoleRender()
+        game = ChessGame(render)
+        game.run()
+
+
+if __name__ == "__main__":
+    player = Player()
+    player.play_chess()
+
+
